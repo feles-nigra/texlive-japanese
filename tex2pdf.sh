b57e01028883bea820000af55f094cca67b84679
h-ito <Hiroshi.Ito@nabiace.co.jp>
2025-01-27 16:19:34 +0900

    Modified to supress standard output and display it only a compile error occurs.

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-ptex2pdf -l $1 
-ls | grep -e '.aux$' -e '.nav$' -e '.log$' -e '.out$' -e '.snm$' -e '.toc$' | xargs rm
+file=$1
+
+yes "quit()" | ptex2pdf -l $file > temp.log 2> error.log
+
+
+if grep -q "LaTeX Error" "temp.log"; then
+  cat temp.log > latex_error
+  awk '/Error/{flag=1} flag' temp.log | head
+fi
+
+ls | grep -e 'dvi$' -e '.aux$' -e '.nav$' -e '.log$' -e '.out$' -e '.snm$' -e '.toc$' | xargs rm
