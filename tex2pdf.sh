4cd9416162621f8354a28ad8c24a6ca3ac7a37ad
h-ito <Hiroshi.Ito@nabiace.co.jp>
2025-01-27 12:00:44 +0900

    tex2pdf.shの修正

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-ptex2pdf -l $1
+ptex2pdf -l $1 
 ls | grep -e '.aux$' -e '.nav$' -e '.log$' -e '.out$' -e '.snm$' -e '.toc$' | xargs rm
-#ls | grep  -e '.tex$' -e '.pdf$' -v | xargs rm 
