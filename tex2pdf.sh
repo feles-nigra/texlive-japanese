aa07d870d1f47c658f8b91a55a547a7efb9fcaea
h-ito <Hiroshi.Ito@nabiace.co.jp>
2025-01-28 10:11:15 +0900

    remove latex_error only if it exists

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -8,7 +8,9 @@ if grep -q "!" "temp.log"; then
   cat temp.log > latex_error
   awk '/\!/{flag=1} flag' temp.log | head
 else
-  rm latex_error
+  if [ -f latex_error ]; then
+    rm latex_error
+  fi
 fi
 
 ls | grep -e 'dvi$' -e '.aux$' -e '.nav$' -e '.log$' -e '.out$' -e '.snm$' -e '.toc$' | xargs rm
